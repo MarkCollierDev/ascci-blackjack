9bf95a69f0a9c1eb576878fd7a4676292592c212
Mark <mark.collier11@hotmail.com>
2016-12-08 09:46:51 +0000

    Deck created and shufeling happening

---
+++ deck.js
@@ -0,0 +1,318 @@
+var deck = [
+
+    {
+        'assci': "<pre> _________</br>|A        |</br>|+   *    |</br>|    !    |</br>|  *-+-*  |</br>|    |    |</br>|   ~~~  +|</br>|        V|</br> ~~~~~~~~~</br></pre>",
+        'value': 1,
+        'face': "CA"
+    },
+    {
+        'assci': "<pre> _________</br>|K |/|\\|  |</br>|+ /o,o\\  |</br>|  \\_-_/  |</br>| ~-_-~-_ |</br>|  /~-~\\  |</br>|  \\o`o/ +|</br>|  |\\|/| X|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "CK"
+    },
+    {
+        'assci': "<pre> _________</br>|Q |~~~|  |</br>|+ /o,o\\  |</br>|  \\_-_/  |</br>| _-~+_-~ |</br>|  /~-~\\  |</br>|  \\o`o/ +|</br>|  |___| Q|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "CQ"
+    },
+    {
+        'assci': "<pre> _________</br>|J /~~|_  |</br>|+ | o`,  |</br>|  | -|   |</br>| =~)+(_= |</br>|   |- |  |</br>|  `.o | +|</br>|  ~|__/ P|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "CJ"
+    },
+    {
+        'assci': "<pre> _________</br>|10+   +  |</br>|+   +    |</br>|  +   +  |</br>|         |</br>|  +   +  |</br>|    +   +|</br>|  +   +0l|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "C10"
+    },
+    {
+        'assci': "<pre> _________</br>|9 +   +  |</br>|+        |</br>|  +   +  |</br>|    +    |</br>|  +   +  |</br>|        +|</br>|  +   + 6|</br> ~~~~~~~~~</br></pre>",
+        'value': 9,
+        'face': "C9"
+    },
+    {
+        'assci': "<pre> _________</br>|8 +   +  |</br>|+        |</br>|  +   +  |</br>|         |</br>|  +   +  |</br>|        +|</br>|  +   + 8|</br> ~~~~~~~~~</br></pre>",
+        'value': 8,
+        'face': "C8"
+    },
+    {
+        'assci': "<pre> _________</br>|7        |</br>|+ +   +  |</br>|    +    |</br>|  +   +  |</br>|         |</br>|  +   + +|</br>|        L|</br> ~~~~~~~~~</br></pre>",
+        'value': 7,
+        'face': "C7"
+    },
+    {
+        'assci': "<pre> _________</br>|6        |</br>|+ +   +  |</br>|         |</br>|  +   +  |</br>|         |</br>|  +   + +|</br>|        9|</br> ~~~~~~~~~</br></pre>",
+        'value': 6,
+        'face': "C6"
+    },
+    {
+        'assci': "<pre> _________</br>|5        |</br>|+        |</br>|  +   +  |</br>|    +    |</br>|  +   +  |</br>|        +|</br>|        S|</br> ~~~~~~~~~</br></pre>",
+        'value': 5,
+        'face': "C5"
+    },
+    {
+        'assci': "<pre> _________</br>|4        |</br>|+        |</br>|  +   +  |</br>|         |</br>|  +   +  |</br>|        +|</br>|        b|</br> ~~~~~~~~~</br></pre>",
+        'value': 4,
+        'face': "C4"
+    },
+    {
+        'assci': "<pre> _________</br>|3        |</br>|+   +    |</br>|         |</br>|    +    |</br>|         |</br>|    +   +|</br>|        E|</br> ~~~~~~~~~</br></pre>",
+        'value': 3,
+        'face': "C3"
+    },
+    {
+        'assci': "<pre> _________</br>|2        |</br>|+        |</br>|    +    |</br>|         |</br>|    +    |</br>|        +|</br>|        Z|</br> ~~~~~~~~~</br></pre>",
+        'value': 2,
+        'face': "C2"
+    },
+    {
+        'assci': "<pre> _________</br>|A        |</br>|O  /~\\   |</br>|  / ^ \\  |</br>| (     ) |</br>|  \\ v /  |</br>|   \\_/  O|</br>|        V|</br> ~~~~~~~~~</br></pre>",
+        'value': 1,
+        'face': "DA"
+    },
+    {
+        'assci': "<pre> _________</br>|K |/|\\|  |</br>|O |o.o|  |</br>|   \\v/   |</br>|  XXXXX  |</br>|   /^\\   |</br>|  |o'o| O| </br>|  |\\|/| X|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "DK"
+    },
+    {
+        'assci': "<pre> _________</br>|Q |~~~|  |</br>|O |o.o|  |</br>|   \\v/   |</br>|  XXOXX  |</br>|   /^\\   |</br>|  |o'o| O| </br>|  |___| Q|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "DQ"
+    },
+    {
+        'assci': "<pre> _________</br>|J /~~|_  |</br>|O ( o\\   |</br>|  ! \\l   |</br>| ^^^Xvvv |</br>|   l\\ I  |</br>|   \\o ) O|</br>|  ~|__/ P|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "DJ"
+    },
+    {
+        'assci': "<pre> _________</br>|10O   O  |</br>|O   O    |</br>|  O   O  |</br>|         |</br>|  O   O  |</br>|    O   O|</br>|  O   O0l|</br> ~~~~~~~~~</br></pre>",
+        'value': 10,
+        'face': "D10"
+    },
+    {
+        'assci': "<pre> _________</br>|9 O   O  |</br>|O        |</br>|  O   O  |</br>|    O    |</br>|  O   O  |</br>|        O|</br>|  O   O 6|</br> ~~~~~~~~~</br></pre>",
+        'value': 9,
+        'face': "D9"
+    },
+    {
+        'assci': "<pre> _________</br>|8 O   O  |</br>|O        |</br>|  O   O  |</br>|         |</br>|  O   O  |</br>|        O|</br>|  O   O 8|</br> ~~~~~~~~~</br></pre>",
+        'value': 8,
+        'face': "D8"
+    },
+    {
+        'assci': "<pre> _________</br>|7        |</br>|O O   O  |</br>|    O    |</br>|  O   O  |</br>|         |</br>|  O   O O|</br>|        L|</br> ~~~~~~~~~</br></pre>",
+        'value': 7,
+        'face': "D7"
+    },
+    {
+        'assci': "<pre> _________</br>|6        |</br>|O O   O  |</br>|         |</br>|  O   O  |</br>|         |</br>|  O   O O|</br>|        9|</br> ~~~~~~~~~</br></pre>",
+        'value': 6,
+        'face': "D6"
+    },
+    {
+        'assci': "<pre> _________</br>|5        |</br>|O        |</br>|  O   O  |</br>|    O    |</br>|  O   O  |</br>|        O|</br>|        S|</br> ~~~~~~~~~</br></pre>",
+        'value': 5,
+        'face': "D5"
+    },
+    {
+        'assci': "<pre> _________</br>|4        |</br>|O        |</br>|  O   O  |</br>|         |</br>|  O   O  |</br>|        O|</br>|        b|</br> ~~~~~~~~~</br></pre>",
+        'value': 4,
+        'face': "D4"
+    },
+    {
+        'assci': "<pre> _________</br>|3        |</br>|O   O    |</br>|         |</br>|    O    |</br>|         |</br>|    O   O|</br>|        E|</br> ~~~~~~~~~</br></pre>",
+        'value': 3,
+        'face': "D3"
+    },
+    {
+        'assci': "<pre> _________</br>|2        |</br>|O        |</br>|    O    |</br>|         |</br>|    O    |</br>|        O|</br>|        Z|</br> ~~~~~~~~~</br></pre>",
+        'value': 2,
+        'face': "D2"
+    },
+    {
+        'assci': "<pre> _________</br>|A        |</br>|# _   _  |</br>| / ~V~ \\ |</br>| \\ Bej / |</br>|  \\ # /  |</br>|   `.'  #|  </br>|        V|</br> ~~~~~~~~~</pre>",
+        'value': 1,
+        'face': "HA"
+    },
+    {
+        'assci': "<pre> _________</br>|K |/|\\|  |</br>|# %*,*%  |</br>|  \\_o_/  |</br>| #>-=-<# |</br>|  /~o~\\  |</br>|  %*'*% #| </br>|  |\\|/| X|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "HK"
+    },
+    {
+        'assci': "<pre> _________</br>|Q |~~~|  |</br>|# %*,*%  |</br>|  \\_o_/  |</br>| -=<*>=- |</br>|  /~o~\\  |</br>|  %*'*% #| </br>|  |___| Q|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "HQ"
+    },
+    {
+        'assci': "<pre> _________</br>|J /~~|_  |</br>|# % *`.  |</br>|  % <~   |</br>| %% / %% |</br>|   _> %  |</br>|  `,* % #|</br>|  ~|__/ P|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "HJ"
+    },
+    {
+        'assci': "<pre> _________</br>|10#   #  |</br>|#   #    |</br>|  #   #  |</br>|         |</br>|  #   #  |</br>|    #   #|</br>|  #   #0l|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "H10"
+    },
+    {
+        'assci': "<pre> _________</br>|9 #   #  |</br>|#        |</br>|  #   #  |</br>|    #    |</br>|  #   #  |</br>|        #|</br>|  #   # 6|</br> ~~~~~~~~~</pre>",
+        'value': 9,
+        'face': "H9"
+    },
+    {
+        'assci': "<pre> _________</br>|8 #   #  |</br>|#        |</br>|  #   #  |</br>|         |</br>|  #   #  |</br>|        #|</br>|  #   # 8|</br> ~~~~~~~~~</pre>",
+        'value': 8,
+        'face': "H8"
+    },
+    {
+        'assci': "<pre> _________</br>|7        |</br>|# #   #  |</br>|    #    |</br>|  #   #  |</br>|         |</br>|  #   # #|</br>|        L|</br> ~~~~~~~~~</pre>",
+        'value': 7,
+        'face': "H7"
+    },
+    {
+        'assci': "<pre> _________</br>|6        |</br>|# #   #  |</br>|         |</br>|  #   #  |</br>|         |</br>|  #   # #|</br>|        9|</br> ~~~~~~~~~</pre>",
+        'value': 6,
+        'face': "H6"
+    },
+    {
+        'assci': "<pre> _________</br>|5        |</br>|#        |</br>|  #   #  |</br>|    #    |</br>|  #   #  |</br>|        #|</br>|        S|</br> ~~~~~~~~~</pre>",
+        'value': 5,
+        'face': "H5"
+    },
+    {
+        'assci': "<pre> _________</br>|4        |</br>|#        |</br>|  #   #  |</br>|         |</br>|  #   #  |</br>|        #|</br>|        b|</br> ~~~~~~~~~</pre>",
+        'value': 4,
+        'face': "H4"
+    },
+    {
+        'assci': "<pre> _________</br>|3        |</br>|#   #    |</br>|         |</br>|    #    |</br>|         |</br>|    #   #|</br>|        E|</br> ~~~~~~~~~</pre>",
+        'value': 3,
+        'face': "H3"
+    },
+    {
+        'assci': "<pre> _________</br>|2        |</br>|#        |</br>|    #    |</br>|         |</br>|    #    |</br>|        #|</br>|        Z|</br> ~~~~~~~~~</pre>",
+        'value': 2,
+        'face': "H2"
+    },
+    {
+        'assci': "<pre> _________</br>|A        |</br>|@   *    |</br>|   / \\   |</br>|  /_@_\\  |</br>|    !    |</br>|   ~ ~  @|</br>|        V|</br> ~~~~~~~~~</pre>",
+        'value': 1,
+        'face': "SA"
+    },
+    {
+        'assci': "<pre> _________</br>|K |/|\\|  |</br>|@ \\- -/  |</br>| ! |-|   |</br>|  % I %  |</br>|   |-| ! |</br>|  /- -\\ @|</br>|  |\\|/| X|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "SK"
+    },
+    {
+        'assci': "<pre> _________</br>|Q |~~~|  |</br>|@ \\- -/  |</br>| o |-|   |</br>|  I % I  |</br>|   |-| o |</br>|  /- -\\ @|</br>|  |___| Q|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "SQ"
+    },
+    {
+        'assci': "<pre> _________</br>|J /~~|_  |</br>|@ ! -\\   |</br>|  \\ -!   |</br>|  ',\\',  |</br>|   I- \\  |</br>|   \\- I @|</br>|  ~|__/ P|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "SJ"
+    },
+    {
+        'assci': "<pre> _________</br>|10@   @  |</br>|@   @    |</br>|  @   @  |</br>|         |</br>|  @   @  |</br>|    @   @|</br>|  @   @0l|</br> ~~~~~~~~~</pre>",
+        'value': 10,
+        'face': "S10"
+    },
+    {
+        'assci': "<pre> _________</br>|9 @   @  |</br>|@        |</br>|  @   @  |</br>|    @    |</br>|  @   @  |</br>|        @|</br>|  @   @ 6|</br> ~~~~~~~~~</pre>",
+        'value': 9,
+        'face': "S9"
+    },
+    {
+        'assci': "<pre> _________</br>|8 @   @  |</br>|@        |</br>|  @   @  |</br>|         |</br>|  @   @  |</br>|        @|</br>|  @   @ 8|</br> ~~~~~~~~~</pre>",
+        'value': 8,
+        'face': "S8"
+    },
+    {
+        'assci': "<pre> _________</br>|7        |</br>|@ @   @  |</br>|    @    |</br>|  @   @  |</br>|         |</br>|  @   @ @|</br>|        L|</br> ~~~~~~~~~</pre>",
+        'value': 7,
+        'face': "S7"
+    },
+    {
+        'assci': "<pre> _________</br>|6        |</br>|@ @   @  |</br>|         |</br>|  @   @  |</br>|         |</br>|  @   @ @|</br>|        9|</br> ~~~~~~~~~</pre>",
+        'value': 6,
+        'face': "S6"
+    },
+    {
+        'assci': "<pre> _________</br>|5        |</br>|@        |</br>|  @   @  |</br>|    @    |</br>|  @   @  |</br>|        @|</br>|        S|</br> ~~~~~~~~~</pre>",
+        'value': 5,
+        'face': "S5"
+    },
+    {
+        'assci': "<pre> _________</br>|4        |</br>|@        |</br>|  @   @  |</br>|         |</br>|  @   @  |</br>|        @|</br>|        b|</br> ~~~~~~~~~</pre>",
+        'value': 4,
+        'face': "S4"
+    },
+    {
+        'assci': "<pre> _________</br>|3        |</br>|@   @    |</br>|         |</br>|    @    |</br>|         |</br>|    @   @|</br>|        E|</br> ~~~~~~~~~</pre>",
+        'value': 3,
+        'face': "S3"
+    },
+    {
+        'assci': "<pre> _________</br>|2        |</br>|@        |</br>|    @    |</br>|         |</br>|    @    |</br>|        @|</br>|        Z|</br> ~~~~~~~~~</pre>",
+        'value': 2,
+        'face': "S2"
+    }
+];
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+shuffle(deck);
+
+deck.forEach(function (card) {
+    var html = "<div class='tight col-md-1'>" + card.assci + "</div>";
+    $('.deck').append(html);
+    console.log(html);
+});
+
+console.log(deck.length);
+
+
+
+//                     **********************************
+//                     *** The Bej's cards (TM) v.1.0 ***
+//                     **********************************
+//
+//                              COPYRIGHT NOTE:
+//You may use this set of Bej's cards or any card from this file in your sigs,
+//ascii drawings, programs, computer games or whatever. But no change
+//may be applied to any card itself. Particulary don't remove the 'Bej'
+//text from the ace of hearts. Using more then 8 (eight) cards requires
+//adding a note in a program/graphics (e.g. 'This program uses Bej's cards').
+//When distributing this file don't make ANY changes. No fee can be
+//taken for this file. If you want to collect fee for program that is using
+//this cards contact Bej.
+//In general if you use any part of this file please send me a note to
+//bej@druid.if.uj.edu.pl just for my pleasure.
+
+
+//    Clubs +             Diamonds O          Hearts #            Spades @
+
+
+//    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+//   Red Joker           Black Joker         Empty card
+
+//   _________           _________           _________
+//  |*   ?   *|         |% \|||/ %|         |         |
+//  |J   *    |         |J /~~~\  |         |         |
+//  |O  /|\   |         |O( o o ) |         |         |
+//  |K  |||   |         |K | ! |  |         |         |
+//  |E ' A `  |         |E | v |  |         |         |
+//  |R  / \   |         |R  \_/   |         |         |
+//  |* J   L *|         |%       %|         |         |
+//   ~~~~~~~~~           ~~~~~~~~~           ~~~~~~~~~
